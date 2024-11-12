bb5f3e7f40aaf471b1542eaa575b4dc6be122850
msa45ad <muppidisaiadithya@gmail.com>
2024-11-12 22:18:35 +0530

    Update sorting of singly linked_list.c

--- sorting of singly linked_list.c
+++ sorting of singly linked_list.c
@@ -1,73 +1,82 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
     int data;
     struct node *link;
 };
-void sort(struct node *,int );
-struct node * add(struct node *,int);
+
+void sort(struct node *head);
+struct node *add(struct node *h, int val);
 void display(struct node *head);
-int main()
-{
-    struct node *head,*ptr;
-    head=(struct node *)malloc(sizeof(struct node));
-    int val,num;
-    printf("enter the no of nodes req:\n");
-    scanf("%d",&num);
-    printf("enter the data:\n");
-    scanf("%d",&val);
-    int i;
-    head->data=val;
-    ptr=head;
-    for(i=0;i<num-1;i++)
-    {
-        printf("enter the data:\n");
-        scanf("%d",&val);
-        ptr=add(ptr,val);
+
+int main() {
+    struct node *head, *ptr;
+    head = (struct node *)malloc(sizeof(struct node));
+    int val, num;
+
+    printf("Enter the number of nodes required:\n");
+    scanf("%d", &num);
+
+    if (num <= 0) {
+        printf("Invalid number of nodes.\n");
+        return 0;
     }
+
+    printf("Enter the data:\n");
+    scanf("%d", &val);
+    head->data = val;
+    head->link = NULL;
+    ptr = head;
+
+    for (int i = 1; i < num; i++) {
+        printf("Enter the data:\n");
+        scanf("%d", &val);
+        ptr = add(ptr, val);
+    }
+
+    printf("Original list:\n");
     display(head);
-    sort(head,num);
+
+    sort(head);
+
+    printf("Sorted list:\n");
     display(head);
+
+    return 0;
 }
-struct node * add(struct node *h,int val)
-{
-    struct node *he;
-    he=(struct node *)malloc(sizeof(struct node));
-    he->data=val;
-    he->link=NULL;
-    h->link=he;
-    return he;
+
+struct node *add(struct node *h, int val) {
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = val;
+    newNode->link = NULL;
+    h->link = newNode;
+    return newNode;
 }
-void display(struct node *head)
-{
-    printf("elements in the linked list are\n");
-    while(head!=NULL)
-    {
-        printf("%d\n",head->data);
-        head=head->link;
+
+void display(struct node *head) {
+    printf("Elements in the linked list:\n");
+    while (head != NULL) {
+        printf("%d\n", head->data);
+        head = head->link;
     }
 }
-void sort(struct node *head,int num)
-{
-    struct node *ptr,*p;
-    ptr=head->link;
-    p=head;
-    int i,j,k;
-    for(i=0;i<num-1;i++)
-    {
-        for(j=i;j<num-1;j++)
-        {
-            if((p->data)>(ptr->data))
-            {
-                k=p->data;
-                p->data=ptr->data;
-                ptr->data=k;
+
+void sort(struct node *head) {
+    struct node *p, *ptr;
+    int temp;
+
+    if (head == NULL) return;
+
+    for (p = head; p != NULL; p = p->link) {
+        for (ptr = head; ptr->link != NULL; ptr = ptr->link) {
+            if (ptr->data > ptr->link->data) {
+                temp = ptr->data;
+                ptr->data = ptr->link->data;
+                ptr->link->data = temp;
             }
-            ptr=ptr->link;
         }
-        p=p->link;
-        ptr=p->link;
     }
 }
+
 
